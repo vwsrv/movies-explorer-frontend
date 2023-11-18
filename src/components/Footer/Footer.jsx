354941e5ab6f13924badd6d8c5faa39c3d866583
Bl Bltich <vissar2016@yandex.ru>
2023-11-18 14:55:25 +0300

    feat: footer component added

---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,14 @@
+export default function Footer() {
+    return (
+        <footer className="footer">
+            <p className="footer__project-name">Учебный проект Яндекс.Практикум х BeatFilm.</p>
+            <div className="footer__container">
+                <p className="footer__copyright">© 2020</p>
+                <nav className="footer__links">
+                    <a href="#" className="footer__link">Яндекс.Практикум</a>
+                    <a href="#" className="footer__link">Github</a>
+                </nav>
+            </div>
+        </footer>
+    )
+}
