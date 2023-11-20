55f95a220b8f215e096bec3e9f5f49bc0fc3df15
Bl Bltich <vissar2016@yandex.ru>
2023-11-21 01:02:51 +0300

    feat: MoviesCardList component added

---
+++ src/components/Movies/MoviesCardList/MoviesCardList.jsx
@@ -0,0 +1,20 @@
+import MoviesCard from "../MoviesCard/MoviesCard";
+
+export default function MoviesCardList () {
+    return(
+        <section className="movies">
+            <MoviesCard />
+            <MoviesCard />
+            <MoviesCard />
+            <MoviesCard />
+            <MoviesCard />
+            <MoviesCard />
+            <MoviesCard />
+            <MoviesCard />
+            <MoviesCard />
+            <MoviesCard />
+            <MoviesCard />
+            <MoviesCard />
+        </section>
+    )
+}
