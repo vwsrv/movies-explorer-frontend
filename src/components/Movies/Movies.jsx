b3dd85139260afefb5603e0802907b9ecac5b5e6
Bl Bltich <vissar2016@yandex.ru>
2023-11-21 01:03:25 +0300

    feat: Movies elements added

---
+++ src/components/Movies/Movies.jsx
@@ -0,0 +1,13 @@
+import SearchForm from "./SearchForm/SearchForm";
+import MoviesCardList from "./MoviesCardList/MoviesCardList";
+import Preloader from "./Preloader/Preloader";
+
+export default function Movies() {
+    return(
+        <section className="section__movies">
+            <SearchForm />
+            <MoviesCardList />
+            <Preloader />
+        </section>
+    )
+}
