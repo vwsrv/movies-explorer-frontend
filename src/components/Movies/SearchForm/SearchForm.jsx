9280282971344f189a494843faa4c0f740870017
Bl Bltich <vissar2016@yandex.ru>
2023-11-20 23:07:44 +0300

    feat: SearchForm component added

---
+++ src/components/Movies/SearchForm/SearchForm.jsx
@@ -0,0 +1,16 @@
+export default function SearchForm() {
+  return (
+    <section className="search">
+      <form action="" className="search__form">
+        <label htmlFor="movie-text" className="search__field">
+          <input type="text" className="search__input" placeholder="Фильм" />
+          <button className="search__form-btn"></button>
+        </label>
+      </form>
+        <label htmlFor="search-type" className="search__toggle">
+          <input type="checkbox" className="search__checkbox" id="search-type" />
+          <span className="search__span">Короткометражки</span>
+        </label>
+    </section>
+  );
+}
