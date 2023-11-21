fef8eab97cd095abd28726cc25b6c036255ea5b5
Bl Bltich <vissar2016@yandex.ru>
2023-11-22 01:37:01 +0300

    feat: Profile component added

---
+++ src/components/Profile/Profile.jsx
@@ -0,0 +1,20 @@
+export default function Profile() {
+    return(
+        <div className="profile">
+            <h2 className="profile__title">Привет, Виталий!</h2>
+            <form className="profile__form">
+                <label htmlFor="user-name" className="profile__field">
+                    <p className="profile__input_name">Имя</p>
+                    <input type="text" className="profile__input profile__input_type-name" />
+                </label>
+                <label htmlFor="user-email" className="profile__field">
+                    <p className="profile__input_name">E-mail</p>
+                    <input type="text" className="profile__input profile__input_type-email" />
+                </label>
+                <span className="profile__input-error"></span>
+                <button className="profile__edit-btn">Редактировать</button>
+                <a href="#" className="profile__signout-link">Выйти из аккаунта</a>
+            </form>
+        </div>
+    )
+}
