5e04260a0be46ea47639f4f9f5a25f792e43cf3e
Bl Bltich <vissar2016@yandex.ru>
2023-11-22 22:55:33 +0300

    feat: NotFoundPage component added

---
+++ src/components/NotFoundPage/NotFoundPage.jsx
@@ -0,0 +1,9 @@
+export default function NotFoundPage() {
+    return (
+        <div className="notfound">
+            <h2 className="notfound__title">404</h2>
+            <p className="notfound__subtitle">Страница не найдена</p>
+            <a href="#" className="notfound__link">Назад</a>
+        </div>
+    )
+}
