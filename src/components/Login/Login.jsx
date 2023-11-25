06a2e6afee70ecbc2cdcecb60c6985bb6edd1b72
Bl Bltich <vissar2016@yandex.ru>
2023-11-25 08:19:24 +0300

    refactor: form validation added

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -1,17 +1,70 @@
+import React, { useEffect, useState, useContext } from "react";
 import Auth from "../Auth/Auth";
+import { useForm } from "react-hook-form";
+import { emailAngular } from "../../utils/constants";
 
 export default function Login() {
+  const [userEmail, setUserEmail] = useState("");
+  const [userPassword, setUserPassword] = useState("");
+  const {
+    register,
+    handleSubmit,
+    formState: { errors, isValid },
+  } = useForm({
+    mode: "onChange",
+  });
+
   return (
-    <Auth title="Рады видеть!" buttonText="Войти" authText="Ещё не зарегистрированы?" linkText="Регистрация">
+    <Auth
+      title="Рады видеть!"
+      buttonText="Войти"
+      authText="Ещё не зарегистрированы?"
+      linkText="Регистрация"
+      onSubmit={handleSubmit}
+    >
       <label htmlFor="user" className="auth__field">
         <span className="auth__input_name">E-mail</span>
-        <input type="text" className="auth__input auth__input_type-email" />
-        <span className="auth__input-error auth__input-error_type-name"></span>
+        <input
+          type="text"
+          className="auth__input auth__input_type-email"
+          {...register("email", {
+            required: "Заполните это поле.",
+            pattern: {
+              value: emailAngular,
+              message: "Укажите корректный email.",
+            },
+          })    
+          }
+          onChange={(e) => setUserEmail(e.target.value)}
+          value={userEmail}
+        />
+        {errors?.email && (
+          <span className="auth__input-error auth__input-error_type-name">
+            {errors?.email?.message}
+          </span>
+        )}
       </label>
       <label htmlFor="password" className="auth__field">
         <span className="auth__input_name">Пароль</span>
-        <input type="text" className="auth__input auth__input_type-password" />
-        <span className="auth__input-error auth__input-error_type-name"></span>
+        <input
+          type="text"
+          className="auth__input auth__input_type-password"
+          {...register("password", {
+            required: "Заполните это поле.",
+            minLength: {
+              value: 8,
+              message: "Минимальная длина пароля - 8 символов.",
+            },
+            })
+          }
+          onChange={(e) => setUserPassword(e.target.value)}
+          value={userPassword}
+        />
+        {errors?.password && (
+          <span className="auth__input-error auth__input-error_type-name">
+            {errors?.password?.message}
+          </span>
+        )}
       </label>
     </Auth>
   );
