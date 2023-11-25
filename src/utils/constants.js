aabc342811fcd3c355d2233264cd8b15b83b5d90
Bl Bltich <vissar2016@yandex.ru>
2023-11-25 09:08:35 +0300

    refactor: constants added

---
+++ src/utils/constants.js
@@ -0,0 +1,3 @@
+export const hiddenRoutes = ['/signin', '/signup', '/profile'];
+export const emailAngular = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+export const passwordPattern = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)[a-zA-Z\d@$!%*?&]{8,}$/;
