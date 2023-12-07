432bfabd954d7b19a561409e196c9e672bc032fa
Bl Bltich <vissar2016@yandex.ru>
2023-12-08 01:51:41 +0300

    fix: correct preloader added

--- src/components/Movies/Preloader/Preloader.jsx
+++ src/components/Movies/Preloader/Preloader.jsx
@@ -1,9 +1,14 @@
-export default function Preloader() {
-  return (
-    <div className="preloader">
-      <button type="button" className="preloader-btn">
-        Ещё
-      </button>
-    </div>
-  );
-}
+import React from 'react'
+import './Preloader.css'
+
+const Preloader = () => {
+    return (
+        <div className="preloader">
+            <div className="preloader__container">
+                <span className="preloader__round"></span>
+            </div>
+        </div>
+    )
+};
+
+export default Preloader
