186293bf1e0b9caa1863e3ee52dc2c0bf7c3a61f
Bl Bltich <vissar2016@yandex.ru>
2023-12-08 01:52:03 +0300

    refactor: Movies component refactored

--- src/components/Movies/Movies.jsx
+++ src/components/Movies/Movies.jsx
@@ -2,12 +2,11 @@ import SearchForm from "./SearchForm/SearchForm";
 import MoviesCardList from "./MoviesCardList/MoviesCardList";
 import Preloader from "./Preloader/Preloader";
 
-export default function Movies({ cards }) {
+export default function Movies({ cards, isLoading }) {
   return (
     <main className="movies">
       <SearchForm />
-      <MoviesCardList movieCard={cards} />
-      <Preloader />
+      <MoviesCardList movieCard={cards} isLoading={isLoading} />
     </main>
   );
 }
--- src/components/Movies/MoviesCardList/MoviesCardList.jsx
+++ src/components/Movies/MoviesCardList/MoviesCardList.jsx
@@ -1,17 +1,53 @@
+import { useState } from "react";
+import { useResize } from "../../../hooks/useResize";
+import { MOBILE_SCREEN_SZ } from "../../../utils/screenBreakpoints";
+import Preloader from "../Preloader/Preloader";
 import MoviesCard from "../MoviesCard/MoviesCard";
 
-export default function MoviesCardList({ movieCard }) {
+export default function MoviesCardList({ movieCard, isLoading }) {
+  const { width } = useResize();
+  const [visibleMoviesDefault, setVisibleMoviesDefault] = useState(12);
+  const [visibleMoviesMobile, setVisibleMoviesMobile] = useState(5);
+  const defaultScreenSize = width >= MOBILE_SCREEN_SZ;
+  const visibleMovies = defaultScreenSize
+    ? visibleMoviesDefault
+    : visibleMoviesMobile;
+
+  function loadMoreMovies() {
+    if (defaultScreenSize) {
+      setVisibleMoviesDefault(visibleMoviesDefault + 3);
+    }
+    return setVisibleMoviesMobile(visibleMoviesMobile + 2);
+  }
+
   return (
     <section className="movies-cards">
-      {movieCard.map((card) => (
-        <MoviesCard
-          key={card.id}
-          movieName={card.nameRU}
-          movieLink={card.image.url}
-          movieDuration={card.duration}
-          trailerLink={card.trailerLink}
-        />
-      ))}
+      {isLoading ? (
+        <Preloader />
+      ) : (
+        <div className="movies-cards--items">
+          {movieCard
+            .slice(0, visibleMovies)
+            .map(({ id, nameRU, image, duration, trailerLink }) => (
+              <MoviesCard
+                key={id}
+                movieName={nameRU}
+                movieLink={image.url}
+                movieDuration={duration}
+                trailerLink={trailerLink}
+              />
+            ))}
+        </div>
+      )}
+      {visibleMovies < movieCard.length && (
+        <button
+          type="button"
+          className="movies-cards--btn"
+          onClick={loadMoreMovies}
+        >
+          Ещё
+        </button>
+      )}
     </section>
   );
 }
