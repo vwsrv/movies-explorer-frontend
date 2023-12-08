9e59dc1634007d4442d310bce6ab74e12657b2ac
Bl Bltich <vissar2016@yandex.ru>
2023-12-09 02:52:37 +0300

    feat: filter and search features added

--- src/components/Movies/Movies.jsx
+++ src/components/Movies/Movies.jsx
@@ -1,12 +1,26 @@
 import SearchForm from "./SearchForm/SearchForm";
 import MoviesCardList from "./MoviesCardList/MoviesCardList";
-import Preloader from "./Preloader/Preloader";
+import { useState } from "react";
 
-export default function Movies({ cards, isLoading }) {
+export default function Movies({ cards, isLoading, onSearch }) {
+  const [isMoviesFiltered, setMoviesFiltered] = useState(false);
+
+  function toggleButtonClick() {
+    setMoviesFiltered(document.getElementById("search-type").checked);
+  }
+
+  function searchMovie(text) {
+    onSearch(text);
+  }
+
   return (
     <main className="movies">
-      <SearchForm />
-      <MoviesCardList movieCard={cards} isLoading={isLoading} />
+      <SearchForm onFilterButtonClick={toggleButtonClick} onSearch={searchMovie} />
+      <MoviesCardList
+        moviesCards={cards}
+        isLoading={isLoading}
+        isFiltered={isMoviesFiltered}
+      />
     </main>
   );
 }
--- src/components/Movies/SearchForm/SearchForm.jsx
+++ src/components/Movies/SearchForm/SearchForm.jsx
@@ -1,7 +1,24 @@
-export default function SearchForm() {
+import { useState } from "react";
+
+export default function SearchForm({onFilterButtonClick, onSearch}) {
+  const [movieName, setMovieName] = useState('');
+  
+  function handleFilterButtonClick() {
+    onFilterButtonClick();
+  }
+
+  function handleInputChange(e) {
+    setMovieName(e.target.value);
+  }
+
+  function handleSubmit(e) {
+    e.preventDefault();
+    onSearch(movieName);
+  }
+
   return (
     <div className="search">
-      <form className="search__form">
+      <form className="search__form" onSubmit={handleSubmit}>
         <div className="search__field">
           <input
             id="movie-text"
@@ -9,14 +26,17 @@ export default function SearchForm() {
             className="search__input"
             placeholder="Фильм"
             required={true}
+            value={movieName}
+            onChange={handleInputChange}
           />
-          <button className="search__form-btn" type="button"></button>
+          <button className="search__form-btn" type="submit"></button>
         </div>
         <label htmlFor="search-type" className="search__toggle">
           <input
             type="checkbox"
             className="search__checkbox"
             id="search-type"
+            onClick={handleFilterButtonClick}
           />
           <span className="search__span">Короткометражки</span>
         </label>
