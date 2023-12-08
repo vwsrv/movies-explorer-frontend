044b8e58b5d5437d85d1737c63c90d3f35b0b4ff
Bl Bltich <vissar2016@yandex.ru>
2023-12-09 02:54:13 +0300

    feat: movies content load by api, find movies function added

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,4 +1,6 @@
 import { Routes, Route, useNavigate } from "react-router-dom";
+import { useState, useEffect } from "react";
+import { CurrentUserContext } from "../../contexts/CurrentUserContext";
 import Login from "../Login/Login";
 import Register from "../Register/Register";
 import Header from "../Header/Header";
@@ -8,11 +10,29 @@ import Movies from "../Movies/Movies";
 import SavedMovies from "../SavedMovies/SavedMovies";
 import Profile from "../Profile/Profile";
 import NotFoundPage from "../NotFoundPage/NotFoundPage";
-import { useState } from "react";
+import { movies } from "../../utils/MoviesApi";
 
 export default function App() {
   const navigate = useNavigate();
   const [loggedIn, setIsLoggedIn] = useState(false);
+  const [currentUser, setCurrentUser] = useState({});
+  const [moviesCards, setMoviesCards] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+
+  useEffect(() => {
+    setIsLoading(true);
+    movies
+      .getMoviesCards()
+      .then((res) => {
+        setMoviesCards(res);
+      })
+      .catch((error) => {
+        console.log(`Ошибка загрузки карточек с фильмами ${error}.`);
+      })
+      .finally(() => {
+        setIsLoading(false);
+      });
+  }, []);
 
   function handleLogin() {
     setIsLoggedIn(true);
@@ -21,22 +41,38 @@ export default function App() {
 
   function handleLogout() {
     setIsLoggedIn(false);
-    navigate("/", {replace: true})
+    navigate("/", { replace: true });
+  }
+
+  function handleSearchMovie(text) {
+    const searchedMovies = moviesCards.filter((movie) => {
+      const nameRU = movie.nameRU.toLowerCase();
+      const nameEN = movie.nameEN.toLowerCase();
+      const searchText = text.toLowerCase();
+
+      return nameRU.includes(searchText) || nameEN.includes(searchText);
+    });
+    setMoviesCards(searchedMovies);
   }
 
   return (
     <div className="page">
-      <Header loggedIn={loggedIn} />
-      <Routes>
-        <Route path="*" element={<NotFoundPage />} />
-        <Route path="/" element={<Main />} />
-        <Route path="movies" element={<Movies />} />
-        <Route path="saved-movies" element={<SavedMovies />} />
-        <Route path="profile" element={<Profile onLogout={handleLogout}/>} />
-        <Route path="signin" element={<Login onLogin={handleLogin} />} />
-        <Route path="signup" element={<Register />} />
-      </Routes>
-      <Footer />
-      </div>
+      <CurrentUserContext.Provider value={currentUser}>
+        <Header loggedIn={loggedIn} />
+        <Routes>
+          <Route path="*" element={<NotFoundPage />} />
+          <Route path="/" element={<Main />} />
+          <Route
+            path="movies"
+            element={<Movies cards={moviesCards} isLoading={isLoading} onSearch={handleSearchMovie} />}
+          />
+          <Route path="saved-movies" element={<SavedMovies />} />
+          <Route path="profile" element={<Profile onLogout={handleLogout} />} />
+          <Route path="signin" element={<Login onLogin={handleLogin} />} />
+          <Route path="signup" element={<Register />} />
+        </Routes>
+        <Footer />
+      </CurrentUserContext.Provider>
+    </div>
   );
 }
