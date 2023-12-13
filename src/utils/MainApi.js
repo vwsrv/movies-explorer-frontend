fff0ef8aac82683cf10f1a2651a18adf57c4ec8c
Bl Bltich <vissar2016@yandex.ru>
2023-12-14 02:12:55 +0300

    feat: save/delete/get-movies requests added

--- src/utils/MainApi.js
+++ src/utils/MainApi.js
@@ -54,12 +54,56 @@ class MainApi {
       return this._checkServerResponse(res);
     });
   }
+
+  saveMovie(movieData) {
+    return fetch(`${this._baseUrl}/movies`, {
+      method: "POST",
+      headers: this._headers,
+      credentials: this._credentials,
+      body: JSON.stringify({
+        country: movieData.country,
+        director: movieData.director,
+        duration: movieData.duration,
+        year: movieData.year,
+        description: movieData.description,
+        image: `https://api.nomoreparties.co${movieData.image.url}`,
+        trailerLink: movieData.trailerLink,
+        thumbnail: `https://api.nomoreparties.co${movieData.image.formats.thumbnail.url}`,
+        movieId: movieData.id,
+        nameRU: movieData.nameRU,
+        nameEN: movieData.nameEN,
+      }),
+    }).then((res) => {
+      return this._checkServerResponse(res);
+    });
+  }
+
+  deleteMovie(movie) {
+    return fetch(`${this._baseUrl}/movies/${movie}`, {
+      method: "DELETE",
+      credentials: this._credentials,
+      headers: this._headers,
+    }).then((res) => {
+      return this._checkServerResponse(res);
+    });
+  }
+
+  getSavedMoviesCards() {
+    return fetch(`${this._baseUrl}/movies`, {
+      method: "GET",
+      credentials: this._credentials,
+      headers: this._headers,
+    }).then((res) => {
+      return this._checkServerResponse(res);
+    });
+  }
 }
 
 export const userApi = new MainApi({
   baseUrl: "http://localhost:3000",
-  headers: { 
-    "Accept" : "application/json",
-    "Content-Type": "application/json" },
+  headers: {
+    Accept: "application/json",
+    "Content-Type": "application/json",
+  },
   credentials: "include",
 });
