f41d116b330cf0364fb13ce61abbc443318326e8
Bl Bltich <vissar2016@yandex.ru>
2023-12-14 21:22:14 +0300

    feat: custom hook added

---
+++ src/hooks/useLocalStorage.js
@@ -0,0 +1,23 @@
+import { useState, useEffect } from "react";
+
+function isDefined(storedValue) {
+    return storedValue !== null && storedValue !== 'undefined';
+}
+
+export default function useLocalSotageState(key, initialValue) {
+    const [state, setState] = useState(() => {
+        const storedValue = localStorage.getItem(key);
+
+        if (isDefined(storedValue)) {
+            return JSON.parse(storedValue)
+        } else {
+            return initialValue;
+        }
+    })
+
+    useEffect(() => {
+        localStorage.setItem(key, JSON.stringify(state))
+    }, [key, state])
+
+    return [state, setState] 
+}
