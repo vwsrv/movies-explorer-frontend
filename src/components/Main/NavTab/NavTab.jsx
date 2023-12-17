196ce53d4f59119a715285d2dca2ebde6aa2b461
Bl Bltich <vissar2016@yandex.ru>
2023-12-17 16:32:11 +0300

    fix: anchor links are fixed

--- src/components/Main/NavTab/NavTab.jsx
+++ src/components/Main/NavTab/NavTab.jsx
@@ -1,19 +1,23 @@
-import { NavLink } from "react-router-dom";
-
 export default function NavTab() {
   return (
     <div className="navtab">
-      <nav className="navtab__navigation">
-        <NavLink href="#project" className="navtab__link navtab__link--about">
-          О проекте
-        </NavLink>
-        <NavLink href="#tech" className="navtab__link  navtab__link--tech">
-          Технологии
-        </NavLink>
-        <NavLink href="#about" className="navtab__link navtab__link--user">
-          Студент
-        </NavLink>
-      </nav>
+      <ul className="navtab__navigation">
+        <li className="navtab__link">
+          <a href="#project" className="navtab__link--item">
+            О проекте
+          </a>
+        </li>
+        <li className="navtab__link">
+          <a href="#tech" className="navtab__link--item">
+            Технологии
+          </a>
+        </li>
+        <li className="navtab__link">
+          <a href="#about" className="navtab__link--item">
+            Студент
+          </a>
+        </li>
+      </ul>
     </div>
   );
 }
