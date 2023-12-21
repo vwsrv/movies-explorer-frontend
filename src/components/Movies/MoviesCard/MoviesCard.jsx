c2df8c6512f552a6b4573012c2c28132e63e6563
Bl Bltich <vissar2016@yandex.ru>
2023-12-22 00:35:30 +0300

    fix: deleted unneccesary functions

--- src/components/Movies/MoviesCard/MoviesCard.jsx
+++ src/components/Movies/MoviesCard/MoviesCard.jsx
@@ -1,29 +1,23 @@
 import { Link } from "react-router-dom";
-import { useEffect } from "react";
-import { useState } from "react";
-import { useLocation } from "react-router-dom";
 
-export default function MoviesCard({ movie, savedMovies, onSave, onDelete }) {
-  const [isSaved, setIsSaved] = useState(false);
+export default function MoviesCard({
+  movie,
+  savedMoviesPath,
+  savedMovies,
+  onSave,
+  onDelete,
+}) {
+  //проверка фильмов на наличие в сохраненных
+  const isSaved = savedMovies.some(
+    (savedMovie) => savedMovie.movieId === movie.id
+  );
   const filmSaveButtonClassName = `film__like-btn ${
     isSaved && `film__like-btn_active`
   }`;
   const hours = Math.floor(movie.duration / 60);
   const mins = Math.round(movie.duration % 60);
-  const location = useLocation();
-  const savedMoviesPath = location.pathname === "/saved-movies";
-
-  useEffect(() => {
-    const isMovieSaved = savedMovies.some(
-      (savedMovie) => savedMovie.movieId === movie.id
-    );
-    if (isMovieSaved) {
-      setIsSaved(isMovieSaved);
-    } else {
-      setIsSaved(false)
-    }
-  }, [movie, savedMovies]);
-
+  //поиск в массиве сохраненых фильмов, сравнение ИД найденного элемента ИД
+  //карточки, полученной с сервера. В случае успеха - добавление свойства _id этой карточке
   function findSavedMovieId(savedMovies) {
     const foundMovie = savedMovies.find(
       (savedMovie) => savedMovie.movieId === movie.id
@@ -36,11 +30,15 @@ export default function MoviesCard({ movie, savedMovies, onSave, onDelete }) {
   }
 
   function toggleMovieStatus() {
+    // Поиск сохраненного фильма
     const foundMovie = findSavedMovieId(savedMovies);
-    if (foundMovie) {
-      return onDelete(foundMovie);
+    // Если фильм найден или мы находимся на странице /saved-movies, вызываем onDelete
+    if (foundMovie || savedMoviesPath) {
+      onDelete(foundMovie ? foundMovie : movie._id);
+    } else {
+      // В противном случае, вызываем onSave
+      onSave(movie);
     }
-    onSave(movie);
   }
 
   return (
@@ -65,8 +63,8 @@ export default function MoviesCard({ movie, savedMovies, onSave, onDelete }) {
       <li className="film__info">
         <h2 className="film__title">{movie.nameRU}</h2>
         <button
-          onClick={toggleMovieStatus}
           type="button"
+          onClick={toggleMovieStatus}
           className={
             isSaved || !savedMoviesPath
               ? filmSaveButtonClassName
