193e17ff21cb15c5ed892d8013eee6ee28870ff4
Bl Bltich <vissar2016@yandex.ru>
2023-12-22 00:34:06 +0300

    refactor: Login copmonent refactored, ValidationInput added

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -1,101 +1,78 @@
-import { useState } from "react";
-import Auth from "../Auth/Auth";
-import { useForm } from "react-hook-form";
+import { useState, useRef } from "react";
+import { useForm, FormProvider } from "react-hook-form";
 import { emailAngular } from "../../utils/constants";
 import { useEffect } from "react";
+import Auth from "../Auth/Auth";
+import ValidationInput from "../ValidationInput/ValidationInput";
 
-export default function Login({ onLogin, connectionError }) {
-  const [userEmail, setUserEmail] = useState("");
-  const [userPassword, setUserPassword] = useState("");
-  const [error, setError] = useState("");
-  const {
-    register,
-    handleSubmit,
-    formState: { errors, isValid },
-  } = useForm({
-    mode: "all",
+export default function Login({ onLogin, connectionError, successMessage }) {
+  const [connectionInfo, setConnectionInfo] = useState("");
+  const { ...methods } = useForm({
+    mode: "onChange",
+    defaultValues: {
+      name: "",
+      password: "",
+    },
   });
+  const userEmail = methods.watch("email");
+  const userPassword = methods.watch("password");
 
   useEffect(() => {
-    setError(connectionError);
+    setConnectionInfo(connectionError);
   }, [connectionError]);
 
-  function onSubmit() {
-    onLogin(userEmail, userPassword);
-    if (connectionError) {
-      return setError(connectionError);
-    } else {
-      setError("");
+  useEffect(() => {
+    if (userEmail || userPassword) {
+      setConnectionInfo("");
     }
-  }
+  }, [userEmail, userPassword]);
 
-  function handleInputChange(e) {
-    if (e.target.name === 'email') {
-      setUserEmail(e.target.value);
-    } else if (e.target.name === 'password') {
-      setUserPassword(e.target.value);
+  function onSubmit(data) {
+    if (successMessage) {
+      setConnectionInfo(successMessage);
     }
-    setError('');
+    onLogin(data.email, data.password);
+    setConnectionInfo(connectionError);
   }
 
   return (
-    <Auth
-      name="signin"
-      title="Рады видеть!"
-      buttonText="Войти"
-      authText="Ещё не зарегистрированы?"
-      linkText="Регистрация"
-      onSubmit={handleSubmit(onSubmit)}
-      isValid={isValid}
-      connectionError={error}
-    >
-      <label htmlFor="email-input" className="auth__field">
-        <span className="auth__input-name">E-mail</span>
-        <input
-          type="text"
-          className="auth__input auth__input_type-email"
-          placeholder="E-mail"
-          id="email-input"
-          {...register("email", {
+    <FormProvider {...methods}>
+      <Auth
+        name="signin"
+        title="Рады видеть!"
+        buttonText="Войти"
+        authText="Ещё не зарегистрированы?"
+        linkText="Регистрация"
+        onSubmit={methods.handleSubmit(onSubmit)}
+        connectionError={connectionInfo}
+        isFormValid={methods.formState.isValid}
+      >
+        <ValidationInput
+          inputName="E-mail"
+          inputType="text"
+          name="email"
+          rules={{
             required: "Заполните это поле.",
             pattern: {
               value: emailAngular,
               message: "Укажите корректный email.",
             },
-          })}
-          onChange={handleInputChange}
-          value={userEmail}
+          }}
         />
-        {errors?.email && (
-          <span className="auth__input-error auth__input-error_type-name">
-            {errors?.email?.message}
-          </span>
-        )}
-      </label>
-      <label htmlFor="password-input" className="auth__field">
-        <span className="auth__input-name">Пароль</span>
-        <input
-          type="password"
-          id="password-input"
-          className="auth__input auth__input_type-password"
-          placeholder="Пароль"
-          {...register("password", {
+        <ValidationInput
+          name="password"
+          inputType="password"
+          inputName="Пароль"
+          autoComplete="true"
+          rules={{
             required: "Заполните это поле.",
             minLength: {
               value: 8,
-              message: `Минимальная длина пароля: 8. Вы ввели: ${userPassword.length}.`,
+              message: `Минимальная длина пароля: 8 симв.`,
             },
-          })}
-          onChange={handleInputChange}
-          value={userPassword}
-          autoComplete="on"
+          }}
         />
-        {errors?.password && (
-          <span className="auth__input-error auth__input-error_type-name">
-            {errors?.password?.message || errors?.validate?.message}
-          </span>
-        )}
-      </label>
-    </Auth>
+      </Auth>
+    </FormProvider>
   );
 }
