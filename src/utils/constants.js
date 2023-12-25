632e22f49e8f5babf4971e2f936ed7bd6a2d184b
Bl Bltich <vissar2016@yandex.ru>
2023-12-25 23:13:37 +0300

    feat: constants added, naming fixed

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,3 +1,13 @@
-export const hiddenRoutes = ["/profile", "/saved-movies", "/movies", "/"];
-export const hiddenRoutesFooter = ["/profile"];
-export const emailAngular = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+export const HIDDEN_ROUTES_DEFAULT = [
+  "/profile",
+  "/saved-movies",
+  "/movies",
+  "/",
+];
+export const HIDDEN_ROUTES_FOOTER = ["/profile"];
+export const EMAIL_ANGULAR = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+export const DEFAULT_MOVIES_COUNT = 12;
+export const MOBILE_MOVIES_COUNT = 5;
+export const SHORT_FILM_DURATION = 40;
+export const MOBILE_MOVIES_COUNT_TO_LAOD = 2;
+export const DEFAULT_MOVIES_COUNT_TO_LOAD = 3;
